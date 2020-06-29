eb54d79cbb3d61d4d39afc1f1d65515318f34586
efermenal <ferendher@gmail.com>
2020-06-29 13:15:22 -0400

    1. Scoping and subcomponent in registration flow

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -3,17 +3,24 @@ package com.example.android.dagger.di
 import android.content.Context
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import dagger.BindsInstance
 import dagger.Component
+import javax.inject.Singleton
 
-
-@Component(modules = [StorageModule::class])
+@Singleton
+@Component(modules = [StorageModule::class, AppSubcomponents::class])
 interface AppComponent {
 
     @Component.Factory
     interface Factory{
         fun create(@BindsInstance context: Context) : AppComponent
     }
-    fun inject(activity : RegistrationActivity)
-    fun inject(activity : MainActivity)
+    
+    fun registrationComponent (): RegistrationComponent.Factory
+    
+    fun inject(activity: MainActivity)
+
 }
